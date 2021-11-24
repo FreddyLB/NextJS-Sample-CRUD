66c52c2a694917eda5c79c54383e4deac051d598
Freddy Luis Paredes B <fparedes@mardom.com>
2021-11-24 16:54:13 -0400

    Added imageProxy to ImageWithFallback component

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    minimumCacheTTL: 60,
-    domains: ["www.publicdomainpictures.net", "www.thelist.com", "cdn.mos.cms.futurecdn.net"],
+    minimumCacheTTL: 60
   },
 };
--- src/components/ImageWithFallback.tsx
+++ src/components/ImageWithFallback.tsx
@@ -1,29 +1,44 @@
-import Image, { ImageProps } from "next/image";
+import Image, { ImageLoader, ImageProps } from "next/image";
 import { useRef, useState } from "react";
+import { API_URL } from "src/client/contants";
 
 const NOT_FOUND_IMAGE = "/images/not-found.jpg";
+const IMAGE_PROXY_URL = API_URL + "/imageproxy?url=";
 
-export interface ImageWithFallbackProps extends Omit<ImageProps, 'src'> {
+export interface ImageWithFallbackProps extends Omit<ImageProps, "src"> {
   src?: string;
   fallbackImage?: string;
   alt: string;
+  useProxy?: boolean; 
+}
+
+const imageLoader : ImageLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`
 }
 
 export function ImageWithFallback({
   src,
   alt,
   fallbackImage,
+  useProxy,
   ...rest
 }: ImageWithFallbackProps) {
   fallbackImage = fallbackImage ?? NOT_FOUND_IMAGE;
-  const [imageSrc, setImageSrc] = useState(src);
+  const actualSrc = src || fallbackImage;
+
+  if (useProxy === true && src != null) {
+    src = IMAGE_PROXY_URL + encodeURIComponent(src);
+  }
+
+  const [imageSrc, setImageSrc] = useState(actualSrc);
   const fallbackImageSet = useRef(false);
 
   return (
     <Image
       {...rest}
-      src={imageSrc || fallbackImage}
+      src={imageSrc}
       alt={alt}
+      loader={imageLoader}
       onError={() => {
         // To prevent a loop if the fallback image is not found
         if (fallbackImageSet.current === false) {
